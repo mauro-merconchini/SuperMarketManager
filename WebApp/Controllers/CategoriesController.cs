e9d9c9654ac9d351084484ced81b2374771810b2
Mauro Merconchini <mauro_merconchini@yahoo.com>
2024-02-26 16:19:16 -0500

    CategoriesController with action that can take a parameter

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("CategoriesIndex");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id != null)
+            {
+                return new ContentResult { Content = id.ToString() };
+            }
+            
+            return new ContentResult { Content = "ERROR: null or invalid id" };
+        }
+    }
+}
