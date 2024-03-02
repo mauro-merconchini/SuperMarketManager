8cba1c5f515fb65cb552127cf90d50d79ff3a28f
Mauro Merconchini <mauro_merconchini@yahoo.com>
2024-03-02 17:12:42 -0500

    Custom validation with ValidationAttribute

--- WebApp/ViewModels/SalesViewModel.cs
+++ WebApp/ViewModels/SalesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using WebApp.Models;
+using WebApp.ViewModels.Validations;
 
 namespace WebApp.ViewModels
 {
@@ -11,6 +12,7 @@ namespace WebApp.ViewModels
        
         [Display(Name = "Quantity")]
         [Range(1, int.MaxValue)]
+        [SalesViewModel_EnsureProperQuantity]
         public int QuantityToSell { get; set; }
         public IEnumerable<Category> Categories { get; set; }  = new List<Category>();
         public IEnumerable<Product> Products { get; set;} = new List<Product>();
---
+++ WebApp/ViewModels/Validations/SalesViewModel_EnsureProperQuantity.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+namespace WebApp.ViewModels.Validations
+{
+    public class SalesViewModel_EnsureProperQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var salesViewModel = validationContext.ObjectInstance as SalesViewModel;
+            
+            if (salesViewModel == null)
+            {
+                return new ValidationResult("The sales view model is null.");
+            }
+
+            if (salesViewModel.QuantityToSell <= 0)
+            {
+                return new ValidationResult("The quantity to sell must be greater than zero.");
+            }
+
+            var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+
+            if (product == null)
+            {
+                return new ValidationResult("The selected product doesn't exist.");
+            }
+
+            if (product.Quantity < salesViewModel.QuantityToSell)
+            {
+                return new ValidationResult($"{product.Name} only has {product.Quantity} left.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
